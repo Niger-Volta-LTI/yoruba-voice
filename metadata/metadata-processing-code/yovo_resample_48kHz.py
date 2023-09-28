568e458f05fe1305c7dd42e7ec2c49478269f74f
ruohoruotsi <ruoho.ruotsi@gmail.com>
2023-09-28 12:47:58 -0700

    [ADD] code to resample judiciously

---
+++ metadata/metadata-processing-code/yovo_resample_48kHz.py
@@ -0,0 +1,33 @@
+import glob
+import soundfile as sf
+import resampy
+import librosa
+import os
+
+
+resampled_audio_path = "/Users/iroro/Downloads/yovo/resampled_files/"
+all_audio_path = "/Users/iroro/Downloads/yovo/asr_20230927_FINAL"
+all_audio_list = [x for x in glob.glob(all_audio_path + "/*.wav")]
+
+# Iterate all 20k audio files and verify the sample rate is 48kHz, keep track of counts
+good_sr_count, good_sr = 0, 48000
+bad_sr_count = 0
+for audio_file_path in all_audio_list:
+    # Load in audio file at its native sampling rate
+    x, sr_orig = librosa.load(audio_file_path, sr=None)
+    if int(sr_orig) == good_sr:
+        good_sr_count += 1
+    else:
+        bad_sr_count += 1
+
+        # x is now a 1-d numpy array, with `sr_orig` audio samples per second
+        y = resampy.resample(x, sr_orig, good_sr, filter='kaiser_best')
+
+        # write out new file
+        resampled_file_path = resampled_audio_path + os.path.basename(audio_file_path)
+        print(resampled_file_path)
+        sf.write(resampled_file_path, y, good_sr, subtype='PCM_16')
+
+print("\n-----------------------------------------------------------------------------------------")
+print("good_sr_count: {}  bad_sr_count {}".format(good_sr_count, bad_sr_count))
+print("-------------------------------------------------------------------------------------------")
